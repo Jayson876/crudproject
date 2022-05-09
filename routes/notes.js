7c0557612c45be946f54dc1159e321a78c76d76b
Jason <dljvaz@gmail.com>
2022-05-09 10:43:30 -0500

    commit

--- routes/notes.js
+++ routes/notes.js
@@ -1,12 +1,97 @@
 const express = require("express")
 const router = express.Router()
+const mysqlConnection = require('../database')
 
-    router.get("/", (req, res) =>{
-            res.render("../views/notes.ejs")
+router.get("/", (req, res) =>{
+    mysqlConnection.query("SELECT * FROM crudproject.notes", (err, rows, fields) =>{
+        if(!err)
+        {
+            res.render("../views/notes.ejs", {data: rows})
+        }
+        else
+        {
+            console.log(err);
+        }
+    })
    })
-   
-    router.get("/add", (req, res) =>{
-       res.send("NotesAdd")
+
+
+    router.get("/:id", (req, res) =>{
+    mysqlConnection.query("SELECT * FROM crudproject.projects WHERE id = ?", [req.params.id], (err, rows, fields) =>{
+        if(!err)
+        {
+            res.render("../views/projects.ejs", {data: rows})
+        }
+        else
+        {
+            console.log(err);
+        }
+    })
    })
+
+    router.get("/:id/delete", (req, res) =>{
+    mysqlConnection.query('DELETE FROM crudproject.projects WHERE Id='+ req.params.id, (err, rows, fields) =>{
+        if(!err)
+        {
+            
+            res.redirect('/projects')
+        }
+        else
+        {
+            console.log(err);
+        }
+    })
+   })
+
+
+   router.post('/add', function(req, res) {
+        
+        var sql = "INSERT INTO crudproject.projects (`proj_title`, `proj_desc`, `proj_strt`, `proj_due`) VALUES ('" + req.body.project_title + "', '" + req.body.project_desc + "', '" + req.body.project_strt + "', '" + req.body.project_due + "')"
+        mysqlConnection.query(sql, (err, rows, fields) =>{
+            if(!err)
+            {
+                res.redirect('/projects')
+                
+            }
+            else
+            {
+                console.log(err);
+            }
+        })
+  }); 
+
+  router.get("/edit/:id", (req, res) =>{
+    mysqlConnection.query('SELECT * FROM crudproject.projects WHERE Id='+ req.params.id, (err, rows, fields) =>{
+        if(!err)
+        {
+            res.render("../views/projects-edit.ejs", {data: rows})
+        }
+        else
+        {
+            console.log(err);
+        }
+    })
+   })  
+
+   router.post('/edit/:id', function(req, res) {
+        
+        var sql = "UPDATE crudproject.projects SET proj_title ='" + req.body.project_title + 
+        "', proj_desc ='" + req.body.project_desc + 
+        "', proj_strt ='" +  req.body.project_strt + 
+        "', proj_due ='" + req.body.project_due + 
+        "' WHERE id = " + req.body.id
+        mysqlConnection.query(sql, (err, rows, fields) =>{
+            if(!err)
+            {
+                res.redirect('/projects')
+                
+            }
+            else
+            {
+                console.log(err);
+            }
+        })
+  }); 
+
 
    module.exports = router
